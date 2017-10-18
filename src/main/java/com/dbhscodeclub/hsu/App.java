818c9deb45c3d9dc0b30ed25ea37b4254e5afad7
Kenneth Chen <efeichen@gmail.com>
2017-10-17 21:06:18 -0700

    create project; ignore .idea folder & projectHsu.iml

--- src/main/java/com/dbhscodeclub/hsu/App.java
+++ src/main/java/com/dbhscodeclub/hsu/App.java
@@ -1,4 +1,34 @@
 package com.dbhscodeclub.hsu;
 
-public class App {
+import javafx.application.Application;
+import javafx.application.Application.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    Button btn;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Project Hsu");
+        btn = new Button();
+        btn.setText("Click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(btn);
+
+        Scene scene = new Scene(layout, 300, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
 }
